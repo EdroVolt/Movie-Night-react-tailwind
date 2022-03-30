1b7bee377a051eccd4def186f7726c02ed5a52e5
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-30 23:30:32 +0200

    feat: MovieLis jsx file

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Movie } from "../../APIs/moviesDb.api";
+import Pagination from "../common/Pagination/Pagination";
+import MovieCard from "./MovieCard/MovieCard";
+
+export default function MovieList() {
+  const [movies, setMovies] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const data = await Movie.get("/popular");
+      console.log(data);
+      setTotalPages(data.data.total_pages);
+      setTotalResults(data.data.total_results);
+      setMovies(data.data.results);
+    })();
+  }, []);
+
+  const getData = async (pageNumber) => {
+    const data = await Movie.get("/popular", {
+      params: {
+        page: pageNumber,
+      },
+    });
+
+    setPageNumber(pageNumber);
+    setMovies(data.data.results);
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-12 gap-8">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} id={movie.id} image={movie.poster_path} />
+        ))}
+      </div>
+      <div className="mt-5">
+        <Pagination
+          pageNumber={pageNumber}
+          totalResults={totalResults}
+          totalPages={totalPages}
+          getData={getData}
+        />
+      </div>
+    </>
+  );
+}
